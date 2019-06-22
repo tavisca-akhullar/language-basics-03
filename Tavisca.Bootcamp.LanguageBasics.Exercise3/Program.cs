bfd41a6d0dbd2121499c28732ae6fcca95ee1601
tavisca-akhullar <akhullar@tavisca.in>
2019-06-22 23:08:02 +0530

    Done with language-basics-03

--- Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections;
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
@@ -40,8 +41,135 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int[] SelectMeals(int[] protein, int[] carbs, int[] fat, string[] dietPlans)
         {
-            // Add your code here.
+            int[] output = new int[dietPlans.Length];
+
+            int[] calories = new int[protein.Length];
+            for (int i = 0; i < protein.Length; i++)
+            {
+                calories[i] = fat[i] * 9 + carbs[i] * 5 + protein[i] * 5;
+            }
+
+            for (int i = 0; i < dietPlans.Length; i++)
+            {
+
+                string diet = dietPlans[i];
+
+                if (diet.Length == 0)
+                {
+
+                    output[i] = 0;
+
+                }
+                else
+                {
+                    ArrayList tempList = new ArrayList();
+                    for (int j = 0; j < diet.Length; j++)
+                    {
+                        char[] temp = diet.ToCharArray();
+                        if (temp[j] == 'P')
+                        {
+                            tempList = SelectMealsUtil(protein, tempList, "max");
+                        }
+                        else if (temp[j] == 'p')
+                        {
+                            tempList = SelectMealsUtil(protein, tempList, "min");
+                        }
+                        else if (temp[j] == 'C')
+                        {
+                            tempList = SelectMealsUtil(carbs, tempList, "max");
+                        }
+                        else if (temp[j] == 'c')
+                        {
+                            tempList = SelectMealsUtil(carbs, tempList, "min");
+                        }
+                        else if (temp[j] == 'F')
+                        {
+                            tempList = SelectMealsUtil(fat, tempList, "max");
+                        }
+                        else if (temp[j] == 'f')
+                        {
+                            tempList = SelectMealsUtil(fat, tempList, "min");
+                        }
+                        else if (temp[j] == 'T')
+                        {
+                            tempList = SelectMealsUtil(calories, tempList, "max");
+                        }
+                        else if (temp[j] == 't')
+                        {
+                            tempList = SelectMealsUtil(calories, tempList, "min");
+                        }
+                    }
+                    output[i] = (int)tempList[0];
+                }
+            }
+            return output;
             throw new NotImplementedException();
+        }
+
+
+        public static ArrayList SelectMealsUtil(int[] array, ArrayList list, string maxMin)
+        {
+            if (list.Count == 0)
+            {
+                if (maxMin.Equals("max"))
+                {
+                    int max = int.MinValue;
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        if (array[i] > max) max = array[i];
+                    }
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        if (array[i] == max) list.Add(i);
+                    }
+                }
+                else
+                {
+                    int min = int.MaxValue;
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        if (array[i] < min) min = array[i];
+                    }
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        if (array[i] == min) list.Add(i);
+                    }
+                }
+
+            }
+            else
+            {
+                if (list.Count == 1) { return list; }
+                ArrayList listTemp = new ArrayList();
+                if (maxMin.Equals("max"))
+                {
+                    int max = int.MinValue;
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if (array[(int)list[i]] > max) max = (int)list[i];
+                    }
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if ((int)list[i] == max) listTemp.Add(list[i]);
+                    }
+
+                }
+                else
+                {
+                    int min = int.MaxValue;
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if (array[(int)list[i]] < min) min = (int)list[i];
+                    }
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if ((int)list[i] == min) listTemp.Add(list[i]);
+                    }
+
+                }
+                return listTemp;
+            }
+            return list;
         }
     }
 }
